2cb3809c20c8935fb0b5321f69b9952de0f8a735
Darren Ng <Un1Gfn@gmail.com>
2021-10-05 16:29:23 +0800

    getopt_long()

--- bmp_bin.c
+++ bmp_bin.c
@@ -7,6 +7,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
+#include <stdbool.h>
+#include <ctype.h> // isgraph()
+#include <getopt.h> // struct option
 
 #include "bmp.h"
 
@@ -18,14 +21,24 @@
 #define Write2B(V) assert(1==fwrite(&(uint16_t){htole16((uint16_t)(V))},2,1,stdout))
 #define Write4B(V) assert(1==fwrite(&(uint32_t){htole32((uint32_t)(V))},4,1,stdout))
 
-static       uint32_t h=0;
+static const char*    program_name=NULL;
+static       int      upsidedown=false;
+static       uint32_t h=512;
 static       uint32_t binsz=0;
 static       uint32_t w=0;
 static const uint16_t bpp=8; // 1 Byte per pixel
 static       uint32_t pixarrsz=0;
 static       uint32_t filesz=0;
 
-static uint8_t *readbin_setglobvar(){
+// https://github.com/coreutils/coreutils/blob/master/src/uname.c
+static inline int help(){
+  eprintf("Usage: %s [-f|-u|--flip|--upside-down] [-h|--height==512] 0<dump.rom 1>|image.bmp\n",program_name);
+  eprintf("       %s --help\n",program_name);
+  exit(EXIT_FAILURE);
+  return EXIT_FAILURE;
+}
+
+static uint8_t *readbin(){
 
   assert(
     0==fseek(stdin,0,SEEK_END) &&
@@ -37,20 +50,24 @@ static uint8_t *readbin_setglobvar(){
   rewind(stdin);
 
   w=(binsz+h-1)/h;
-  eprintf("%u -> %ux%u\n",binsz,w,h);
+  eprintf("%u - %ux%u - %s",
+    binsz,
+    w,
+    h,
+    upsidedown?"flip":"normal"
+  );
 
   assert(bpp==8);
   pixarrsz=PAD(4,w)*h;
   filesz=PAD(4,BITMAPFILEHEADER+BITMAPINFOHEADER+CTSZ)+pixarrsz;
   eprintf("pixarr(file) %uB(%uB)\n",pixarrsz,filesz);
 
-  // assert(0);
-
   uint8_t *ret=calloc(1,binsz);
   assert(ret);
   assert((long long)binsz==(long long)fread(ret,1,binsz,stdin));
   assert(0==fclose(stdin));
   return ret;
+
 }
 
 static inline void writebfh(){
@@ -138,11 +155,14 @@ static inline void writepixarr(const uint8_t *const bin){
   //     cast[r][c]=cl;
   //   }
 
-  // Bin snapshot
-  for(uint32_t i=0;i<binsz;++i){
-    // cast[r][c]=bin[c*h+r];
-    cast[i%h][i/h]=bin[i];
-  }
+  // Bin snapshot (transpost)
+  if(upsidedown)
+    for(uint32_t i=0;i<binsz;++i)
+      // cast[r][c]=bin[c*h+r];
+      cast[i%h][i/h]=bin[i];
+  else
+    for(uint32_t i=0;i<binsz;++i)
+      cast[h-i%h-1][i/h]=bin[i];
 
   fwrite(pixarr,pixarrsz,1,stdout);
   free(pixarr);pixarr=NULL;
@@ -150,15 +170,75 @@ static inline void writepixarr(const uint8_t *const bin){
 
 }
 
-int main(const int argc,const char *const *const argv){
+static inline void args(const int argc,char *const argv[]){
 
-  assert(argc==2);
-  assert(argv&&argv[1]&&argv[1][0]);
-  h=atoi(argv[1]);
-  assert(1<=h&&h<=4096);
+  // // ! " # $ % & ' ( ) * + , . /
+  // // 0 1 2 3 4 5 6 7 8 9
+  // // < = > ? @
+  // // A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+  // // [ \ ] ^ _ `
+  // // a b c d e f g h i j k l m n o p q r s t u v w x y z
+  // // { | } ~
+  // for(int i=0x00;i<=0xFF;++i){
+  //   if( i!='-' && i!=':' && i!=';' && 0!=isgraph(i) )
+  //     printf("%c ",i);
+  // }
 
+  (argc<=1)?help():0;
+
+  // int height_found=0;
+  const struct option longopts[]={
+    {"flip"       ,no_argument      ,NULL,'f'},
+    {"upside-down",no_argument      ,NULL,'u'},
+    {"height"     ,required_argument,NULL,'h'},
+    {"help"       ,no_argument      ,NULL, 0 },
+    {NULL,0,NULL,0}
+  };
+
+  optind=0; // getopt(3)#NOTES
+  assert(opterr==1);
+  int ch=0;
+  int longindex=-1;
+  while(-1!=(ch=getopt_long(argc,argv,"+""fu""h:",longopts,&longindex)))switch(ch){
+    case 'f':
+    case 'u':
+      assert(!optarg);upsidedown=true;break;
+    case 'h':
+      assert(optarg&&optarg[0]);h=atoi(optarg);break;
+    case '?':
+    case  0 :
+      assert(!optarg);help();break;
+    // case 0:switch(longindex){
+    //     case 0:assert(upsidedown);break;
+    //     case 1:assert(upsidedown);break;
+    //     case 2:assert(height_found);break;
+    //     default:assert(0);break;
+    //   }break;
+    default:
+      // printf("%c",ch);
+      // if(optarg&&optarg[0])printf(" = %s",optarg);
+      assert(0);break;
+  }
+
+  assert(optind==argc);
+
+}
+
+int main(const int argc,char *const argv[]){
+
+  program_name=argv[0];
+  args(argc,argv);
+  assert((int32_t)h>0);
+  uint32_t tmp=h;
+  for(;;){
+    if(tmp==128)break;
+    assert(tmp>128);
+    tmp/=2;
+  }
+
+  // assert(0);
   assert(!isatty(STDIN_FILENO));
-  uint8_t *bin=readbin_setglobvar();
+  uint8_t *bin=readbin();
 
   assert(!isatty(STDOUT_FILENO));
   writebfh();
